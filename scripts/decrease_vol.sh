a79b465c02eea6fc735fa5e8c0a13b24210dd5b1
David Liu <davidas99017@gmail.com>
2025-03-05 16:30:30 -0500

    init

---
+++ scripts/decrease_vol.sh
@@ -0,0 +1,12 @@
+
+#!/bin/bash
+
+ags toggle osd
+
+sleep 0.1
+
+wpctl set-volume -l 1 @DEFAULT_AUDIO_SINK@ 5%- 
+
+sleep 5
+
+ags toggle osd
---
+++ scripts/pickwall.sh
@@ -0,0 +1,42 @@
+#!/usr/bin/env bash
+
+XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
+CONFIG_DIR="$XDG_CONFIG_HOME/ags"
+
+switch() {
+	imgpath=$1
+	read scale screenx screeny screensizey < <(hyprctl monitors -j | jq '.[] | select(.focused) | .scale, .x, .y, .height' | xargs)
+	cursorposx=$(hyprctl cursorpos -j | jq '.x' 2>/dev/null) || cursorposx=960
+	cursorposx=$(bc <<< "scale=0; ($cursorposx - $screenx) * $scale / 1")
+	cursorposy=$(hyprctl cursorpos -j | jq '.y' 2>/dev/null) || cursorposy=540
+	cursorposy=$(bc <<< "scale=0; ($cursorposy - $screeny) * $scale / 1")
+	cursorposy_inverted=$((screensizey - cursorposy))
+
+	if [ "$imgpath" == '' ]; then
+		echo 'Aborted'
+		exit 0
+	fi
+
+	# ags run-js "wallpaper.set('')"
+	# sleep 0.1 && ags run-js "wallpaper.set('${imgpath}')" &
+	swww img "$imgpath" --transition-step 100 --transition-fps 120 \
+		--transition-type grow --transition-angle 30 --transition-duration 2 \
+		--transition-pos "$cursorposx, $cursorposy_inverted"
+  
+  matugen image $imgpath -t scheme-tonal-spot
+  kill -SIGUSR1 $(pidof kitty)
+}
+
+if [ "$1" == "--noswitch" ]; then
+	imgpath=$(swww query | awk -F 'image: ' '{print $2}')
+	# imgpath=$(ags run-js 'wallpaper.get(0)')
+elif [[ "$1" ]]; then
+	switch "$1"
+  # generate_color "$1"
+else
+	# Select and set image (hyprland)
+
+    cd "$(xdg-user-dir PICTURES)" || return 1
+	switch "$(yad --width 1200 --height 800 --file --add-preview --large-preview --title='Choose wallpaper')"
+fi
+
