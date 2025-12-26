c37f003995c6e0b952347ad223d1ec0d0611da2c
David Liu <davidas99017@gmail.com>
2025-12-26 01:57:24 -0800

    Updated to accommodate the new shell

--- scripts/pickwall.sh
+++ scripts/pickwall.sh
@@ -4,28 +4,27 @@ XDG_CONFIG_HOME="${XDG_CONFIG_HOME:-$HOME/.config}"
 # CONFIG_DIR="$XDG_CONFIG_HOME/ags"
 
 switch() {
-	imgpath=$1
-	if [ "$imgpath" == '' ]; then
-		echo 'Aborted'
-		exit 0
-	fi
+  imgpath=$1
+  if [ "$imgpath" == '' ]; then
+    echo 'Aborted'
+    exit 0
+  fi
 
-	swww img "$imgpath"
+  swww img "$imgpath"
 
   matugen image $imgpath -t scheme-tonal-spot
   kill -SIGUSR1 $(pidof kitty)
 }
 
 if [ "$1" == "--noswitch" ]; then
-	imgpath=$(swww query | awk -F 'image: ' '{print $2}')
-	# imgpath=$(ags run-js 'wallpaper.get(0)')
+  imgpath=$(swww query | awk -F 'image: ' '{print $2}')
+  # imgpath=$(ags run-js 'wallpaper.get(0)')
 elif [[ "$1" ]]; then
-	switch "$1"
+  switch "$1"
   # generate_color "$1"
 else
-	# Select and set image (hyprland)
+  # Select and set image (hyprland)
 
-    cd "$(xdg-user-dir PICTURES)" || return 1
-	switch "$(yad --width 1200 --height 800 --file --add-preview --large-preview --title='Choose wallpaper')"
+  cd "$(xdg-user-dir PICTURES)" || return 1
+  switch "$(yad --width 1200 --height 800 --file --add-preview --large-preview --title='Choose wallpaper')"
 fi
-
